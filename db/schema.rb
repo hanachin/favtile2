526a84ae9b13a72b9e8f526cda9d1e900eb7c00b
Seiei Higa <hanachin@gmail.com>
2013-09-23 15:47:58 +0900

    Setup hstore extension

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,11 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20130923060125) do
+ActiveRecord::Schema.define(version: 20130923063854) do
+
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+  enable_extension "hstore"
 
   create_table "users", force: true do |t|
     t.string   "name"
