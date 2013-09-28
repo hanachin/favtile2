7fe0f7642383505c4607fe5537459e258cabdbb7
Seiei Higa <hanachin@gmail.com>
2013-09-29 07:17:49 +0900

    Clean code

--- spec/acceptance/steps/twitter_steps.rb
+++ spec/acceptance/steps/twitter_steps.rb
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 step ":twitter_name が :tweet をふぁぼった" do |twitter_name, tweet|
-  @tweets ||= {}
-  @tweets[twitter_name] ||= []
-  @tweets[twitter_name] << tweet
+  @favorites_tweets ||= {}
+  @favorites_tweets[twitter_name] ||= []
+  @favorites_tweets[twitter_name] << tweet
 end
